4cb2cd716854843042db4ccf84778a67faffed78
Jake Soenneker <jake@soenneker.com>
2025-03-26 12:35:30 -0500

    Tests

--- test/Soenneker.Attributes.Validation.Guid.Tests/GuidValidationAttributeTests.cs
+++ test/Soenneker.Attributes.Validation.Guid.Tests/GuidValidationAttributeTests.cs
@@ -1,4 +1,5 @@
-﻿using Soenneker.Tests.Unit;
+﻿using FluentAssertions;
+using Soenneker.Tests.Unit;
 using Xunit;
 
 namespace Soenneker.Attributes.Validation.Guid.Tests;
@@ -9,5 +10,53 @@ public sealed class GuidValidationAttributeTests : UnitTest
     public void Default()
     {
 
+    }
+
+    [Fact]
+    public void Valid_guid_should_validate()
+    {
+        var attribute = new GuidValidationAttribute();
+
+        var value = Faker.Random.Guid().ToString();
+
+        bool result = attribute.IsValid(value);
+
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Null_value_should_not_validate()
+    {
+        var attribute = new GuidValidationAttribute();
+
+        string? value = null;
+
+        bool result = attribute.IsValid(value);
+
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void Invalid_guid_should_not_validate()
+    {
+        var attribute = new GuidValidationAttribute();
+
+        string value = Faker.Random.String(5);
+
+        bool result = attribute.IsValid(value);
+
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void Empty_guid_should_not_validate()
+    {
+        var attribute = new GuidValidationAttribute();
+
+        var value = System.Guid.Empty.ToString();
+
+        bool result = attribute.IsValid(value);
+
+        result.Should().BeFalse();
     }
 }
